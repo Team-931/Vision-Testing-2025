c1cd37e043651f52cd4ffec5b85e4542b531713b
A564790 <catsandogs3300@gmail.com>
2025-01-14 17:00:13 -0600

    Updated tuning values and updated the yagsil version

--- Vision-Testing-2025/src/main/java/frc/robot/RobotContainer.java
+++ Vision-Testing-2025/src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,40 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.Commands.AbsoluteDriveAdv;
+import frc.robot.Subsystems.PoseEstimator;
+import frc.robot.Subsystems.Drivetrain.Drivetrain;
 
 public class RobotContainer {
+  private final CommandXboxController driver;
+  private final Drivetrain drivetrain;
+  private final PoseEstimator poseEstimator;
+  
+  AbsoluteDriveAdv closedAbsoluteDriveAdv;
+
   public RobotContainer() {
+    driver = new CommandXboxController(0);
+    drivetrain = new Drivetrain(new File(Filesystem.getDeployDirectory(),"swerve"));
+    poseEstimator = new PoseEstimator(drivetrain);
+    new AbsoluteDriveAdv(drivetrain,
+                                                                 () -> -MathUtil.applyDeadband(driver.getLeftY(),
+                                                                                               OperatorConstants.LEFT_Y_DEADBAND),
+                                                                 () -> -MathUtil.applyDeadband(driver.getLeftX(),
+                                                                                               OperatorConstants.DEADBAND),
+                                                                 () -> -MathUtil.applyDeadband(driver.getRightX(),
+                                                                                               OperatorConstants.RIGHT_X_DEADBAND),
+                                                                 driver.getHID()::getYButtonPressed,
+                                                                 driver.getHID()::getAButtonPressed,
+                                                                 driver.getHID()::getXButtonPressed,
+                                                                 driver.getHID()::getBButtonPressed);
     configureBindings();
   }
 
