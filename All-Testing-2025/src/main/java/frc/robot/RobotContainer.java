6c1e91d1082be956fc6b7582c456f9dacf947a0c
Philip Harrington <71712.2415@compuserve.com>
2025-02-08 15:13:20 -0600

    Creating All-Testing-2025 project, with drivetrain commented out for superstructure work

---
+++ All-Testing-2025/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.Commands.AbsoluteDriveAdv;
+import frc.robot.Subsystems.PoseEstimator;
+import frc.robot.Subsystems.Drivetrain.Drivetrain;
+
+public class RobotContainer {
+  private final CommandXboxController driver, operator;
+  /* private final Drivetrain drivetrain;
+  private final PoseEstimator poseEstimator;
+   */
+  AbsoluteDriveAdv closedAbsoluteDriveAdv;
+
+  public RobotContainer() {
+    driver = new CommandXboxController(0);
+    operator = new CommandXboxController(0);
+    /* drivetrain = new Drivetrain(new File(Filesystem.getDeployDirectory(),"swerve"));
+    poseEstimator = new PoseEstimator(drivetrain);
+    new AbsoluteDriveAdv(drivetrain,
+                                                                 () -> -MathUtil.applyDeadband(driver.getLeftY(),
+                                                                                               OperatorConstants.LEFT_Y_DEADBAND),
+                                                                 () -> -MathUtil.applyDeadband(driver.getLeftX(),
+                                                                                               OperatorConstants.DEADBAND),
+                                                                 () -> -MathUtil.applyDeadband(driver.getRightX(),
+                                                                                               OperatorConstants.RIGHT_X_DEADBAND),
+                                                                 driver.getHID()::getYButtonPressed,
+                                                                 driver.getHID()::getAButtonPressed,
+                                                                 driver.getHID()::getXButtonPressed,
+                                                                 driver.getHID()::getBButtonPressed);
+     */
+    // TODO: add operator subsusystems.
+    configureBindings();
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
